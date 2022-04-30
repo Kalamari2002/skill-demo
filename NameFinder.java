2ff1e7d9e62433b846c7f2d792dec19fe40f705b
Kalamari2002 <gabriel.ikezaki@gmail.com>
2022-04-30 01:39:09 -0700

    Implemented NameFinder class

--- NameFinder.java
+++ NameFinder.java
@@ -1,3 +1,11 @@
 public class NameFinder{
+    public boolean FindName(String[] names, String name){
+        for(int i = 0; i < names.length; i++){
+            if(name.equals(names[i])){
+                return true;
+            }
+        }
 
+        return false;
+    }
 }
